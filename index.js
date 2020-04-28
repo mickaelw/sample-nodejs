56dd04fa8f14d77a517f4eff8d26d75147b828b8
Petr Kokorev <pkokorev@iterial.ch>
2020-04-28 14:14:00 +0200

    Initial import

---
+++ index.js
@@ -0,0 +1,56 @@
+const amqp = require('amqplib/callback_api');
+const amqpCredentials = require('amqplib').credentials;
+const proto = require('protobufjs');
+
+const fs = require('fs');
+
+const candidatesQueueName = 'q-chatbot_candidates--poc';
+const settingsPath = 'res/settings.json';
+const candidateProtoPath = 'res/candidate.proto';
+
+const settingsString = fs.readFileSync(settingsPath, 'utf-8');
+const settings = JSON.parse(settingsString);
+
+let CandidateType = null;
+let CandidateActionEnum = null;
+proto.load(candidateProtoPath, (error, root) => {
+    CandidateType = root.lookupType('chatbot.Candidate');
+    CandidateActionEnum = root.lookupEnum('chatbot.Action');
+});
+
+const connectionOptions = {credentials: amqpCredentials.plain(settings.username, settings.password)};
+amqp.connect(`amqp://${settings.host}:${settings.port}/${settings.vHost}`, connectionOptions, (connectionError, connection) => {
+
+    if (connectionError) {
+        console.error('Unable to connect to Chatbot MQ server', connectionError);
+    } else {
+        connection.createChannel((channelError, channel) => {
+            if (channelError) {
+                console.error('Unable to create channel', channelError);
+            } else {
+                channel.assertQueue(candidatesQueueName, {durable: true});
+
+                console.log(`Listening to candidate events on queue '${candidatesQueueName}'`);
+
+                channel.consume(candidatesQueueName, message => {
+                    console.log('Candidate event received');
+
+                    if (CandidateType && CandidateActionEnum) {
+                        try {
+                            const candidate = CandidateType.decode(message.content);
+
+                            console.log('Candidate action', CandidateActionEnum.valuesById[candidate.action]);
+                            console.log('Candidate data', candidate);
+
+                            return channel.ack(message);
+                        } catch (e) {
+                            console.log('Cannot decode candidate from event', e);
+                        }
+                    }
+
+                    return channel.nack(message);
+                }, {noAck: false});
+            }
+        });
+    }
+});
